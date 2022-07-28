e7aab7cf0c122653575ceadd0058ce4c7187c49e
Alexsandro <mosfethusa@gmail.com>
2022-07-27 21:07:17 -0300

    Trabalhando com arquivos estaticos com HTML,CSS e NODEJS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express"); // ativo a funcao do express
 const app = express();              // importo o express para dentro da variavel app
 
 app.set('view engine','ejs');       // Estou dizendo para o express para usar o EJS como View engine para desenhar o meu HTML
-
+app.use(express.static('public'));
 
 
 app.get("/:nome/:lang",(req, res)=>{              // Rota padrao ao acessar a rota solicito dois parametos ao usuario
